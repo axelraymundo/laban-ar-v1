2237b80d9b9b8d1e507d662b3748521e4f00d7ee
Axel Raymundo <stephen.raymundo@gmail.com>
2025-03-06 02:28:29 +0800

    version 1.4.2

--- js/aframe-gif-shader.js
+++ js/aframe-gif-shader.js
@@ -230,9 +230,10 @@
       init(data) {
         log("init", data);
         log(this.el.components);
+        var srcElement = document.querySelector(data.src);
         this.__cnv = document.createElement("canvas");
-        //this.__cnv.width = 2;
-        //this.__cnv.height = 2;
+        this.__cnv.width = srcElement.width;
+        this.__cnv.height = srcElement.height;
         this.__ctx = this.__cnv.getContext("2d");
         this.__texture = new THREE.Texture(this.__cnv); //renders straight from a canvas
         if (data.repeat) {
